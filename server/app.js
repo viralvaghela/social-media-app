018c4b6f4fed0ee1a41bd967b26d0454d83df58a
viralvaghela <viralvaghela7109@gmail.com>
2020-09-24 13:27:42 +0530

    email exists checked

--- server/app.js
+++ server/app.js
@@ -16,12 +16,7 @@ app.use("/login", loginRoute);
 app.get("/", (req, res) => {
   // console.log("Welcome to Home");
   res.send("Welcome to Home");
+
 });
 
-//DB connection
-mongoose.connect(
-  "mongodb+srv://viral:viral@socialmedia.ulna4.mongodb.net/<?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => console.log("connected")
-);
 app.listen(5000);
--- server/routes/register.js
+++ server/routes/register.js
@@ -6,19 +6,28 @@ router.get("/", (req, res) => {
   res.send("Welcome to /register");
 });
 
-router.post("/",async (req, res) => {
-  //res.json(req.body);
-  try {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    //res.json(user);
-    const savedUser = await user.save();
-    res.send({"token":savedUser._id});
-  } catch (err) {
-    res.status(400).send(err);
+router.post("/", async (req, res) => {
+  var alreadyAvalUser = await User.findOne({
+    email: req.body.email,
+  });
+  console.log(alreadyAvalUser);
+
+  if (alreadyAvalUser == null) {
+    try {
+      const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      //res.json(user);
+      const savedUser = await user.save();
+      res.send({ token: savedUser._id });
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  }
+   else {
+    res.json({ "msg": "Email already exists" });
   }
 });
 
