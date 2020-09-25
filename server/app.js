bfffbef20f0650ddecbd767f85e9a3076366a365
viralvaghela <viralvaghela7109@gmail.com>
2020-09-25 17:30:51 +0530

    navbar added

--- server/app.js
+++ server/app.js
@@ -8,15 +8,31 @@ app.use(bodyParser.urlencoded());
 
 const registerRoute = require("./routes/register");
 const loginRoute = require("./routes/login");
+const User = require("./models/User");
 
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
+
+//get specific user Data
+app.get("/:userId", async (req, res) => {
+  //res.send(req.params.userId);
+  try {
+    const loggedInUser = await User.findById(req.params.userId);
+    res.json(loggedInUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 //home
 app.get("/", (req, res) => {
   // console.log("Welcome to Home");
   res.send("Welcome to Home");
-
 });
+mongoose.connect(
+  "mongodb://viral:viral@socialmedia-shard-00-00.ulna4.mongodb.net:27017,socialmedia-shard-00-01.ulna4.mongodb.net:27017,socialmedia-shard-00-02.ulna4.mongodb.net:27017/viral?ssl=true&replicaSet=atlas-k5na4e-shard-0&authSource=admin&retryWrites=true&w=majority",
 
+  { useNewUrlParser: true },
+  () => console.log("Connected")
+);
 app.listen(5000);
